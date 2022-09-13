85bf2a7c13d5daab33d573ac643b2e73d5f2c539
regisfaria <regisprogramming@gmail.com>
2022-09-13 20:28:27 +0200

    initial setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
 import express from "express";
+import { purple } from "./shared/lib/chalk";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log(purple("🚀 Server started, running on http://localhost:3000 🚀"));
+});
---
+++ src/shared/lib/chalk.ts
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+
+export const purple = chalk.black.bgMagenta;
